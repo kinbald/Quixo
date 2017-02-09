81e341300fdc6da2ba25d361b5f041080ac054a4
Desrumaux <guillaume.desrumaux@isen.fr>
2017-02-09 22:13:29 +0100

    Fonctions outils

--- App/modules/outils.c
+++ App/modules/outils.c
@@ -4,11 +4,45 @@
 * \author desrumaux
 * \date 24/01/17
 */
-
-#ifndef G_OUTILS
-
-#define G_OUTILS
-
 #include "outils.h"
 
-#endif
+/*!
+ * \brief Accesseur pour un plateau de jeu de type PLATEAU
+ * @param plateau Plateau du jeu à accéder
+ * @param case_jeu Case du jeu à lire
+ * @return ::SYMBOLE du joueur dans la case demandée
+ */
+int getCase(PLATEAU plateau, CASE case_jeu)
+{
+	int valeurRetour;	///< Valeur de retour de la case du plateau demandée
+
+	if ((case_jeu.ligne >= 0) && (case_jeu.ligne < TAILLE_PLATEAU)
+	    && (case_jeu.colonne >= 0) && (case_jeu.colonne < TAILLE_PLATEAU)) {
+		valeurRetour = plateau[case_jeu.ligne][case_jeu.colonne];
+		return (valeurRetour);
+	} else {
+		return (-1);
+	}
+}
+
+/*!
+ * \brief Mutateur pour un plateau de jeu de type PLATEAU
+ * @param plateau Plateau de jeu à modifier
+ * @param case_jeu Case du jeu à modifier
+ * @param symbole Symbole à modifier dans le plateau
+ * @return int
+ */
+int setCase(PLATEAU plateau, CASE case_jeu, SYMBOLE symbole)
+{
+	if ((case_jeu.ligne >= 0)
+	    && (case_jeu.ligne < TAILLE_PLATEAU)
+	    && (case_jeu.colonne >= 0)
+	    && (case_jeu.colonne < TAILLE_PLATEAU)
+	    && (symbole >= croix_bas)
+	    && (symbole <= tampon)) {
+		plateau[case_jeu.ligne][case_jeu.colonne] = symbole;	/// Lecture dans le plateau envoyé
+		return (0);
+	} else {
+		return (-1);
+	}
+}
